da67858a3b0c4d1b613a9c8e62658aab994e2246
Levkov Gleb <Glebchanskiy@yandex.by>
2022-09-15 13:17:02 +0300

    add docs dir

---
+++ LR1/docs/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['loadfromfile_0',['loadFromFile',['../class_matrix.html#ad7842bb1e388d73f7abc7598cac30663',1,'Matrix']]]
+];
---
+++ LR1/docs/html/search/functions_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['operator_2b_2b_0',['operator++',['../class_matrix.html#a8507366342ab66e74b5a14226bf99791',1,'Matrix']]],
+  ['operator_2d_2d_1',['operator--',['../class_matrix.html#ad408cd4553ebed67bfd3fd5cc17f8423',1,'Matrix']]],
+  ['operator_3d_2',['operator=',['../class_matrix.html#abb9a4d91825539b8519b17f1bd96c954',1,'Matrix']]]
+];
