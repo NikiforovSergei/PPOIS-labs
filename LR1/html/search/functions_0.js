3f89fd5fd0d07af5a7f35d3ad7413a51dcb493f2
MchSasha <90107039+MchSasha@users.noreply.github.com>
2022-09-22 12:20:51 +0300

    lab1_Moysevich_121702

---
+++ LR1/html/search/functions_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['getdenom_0',['getDenom',['../class_natural.html#a219e53686da8b45bb333ea8d32eaf59d',1,'Natural']]],
+  ['getint_1',['getInt',['../class_natural.html#a6c65b8db18f56a6f01777c0f1099ef38',1,'Natural']]],
+  ['getnum_2',['getNum',['../class_natural.html#a765f07a4288e5ff5340d36783dc5d066',1,'Natural']]]
+];
