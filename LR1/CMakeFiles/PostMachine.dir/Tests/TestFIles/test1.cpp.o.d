8c99033167a4e36d74bd4d5e31f98f19a0da3143
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2022-09-27 21:45:07 +0300

    rebuild and go to comminism 2.0

--- LR1/CMakeFiles/PostMachine.dir/Tests/TestFIles/test1.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/PostMachine.dir/Tests/TestFIles/test1.cpp.o: \
-  /Users/ardonplay/Developer/PPOIS-labs/LR1/Tests/TestFIles/test1.cpp
--- LR1/CMakeFiles/PostMachine.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PostMachine.
-# This may be replaced when dependencies are built.
