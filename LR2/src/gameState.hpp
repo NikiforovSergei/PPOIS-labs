5d9a7f19d74521e5b3131ca450be5bc8f0863652
HIebyShek <gleb.yakovinich1990@gmail.com>
2022-09-30 20:27:21 +0300

    LR2: patch for gameState

--- LR2/src/gameState.hpp
+++ LR2/src/gameState.hpp
@@ -10,27 +10,28 @@ namespace gameState
         state(/* args */);
         ~state();
 
-        virtual void init() = 0;
+        virtual int init() = 0;
         virtual void cleanup() = 0;
 
         virtual void pause() = 0;
         virtual void resume() = 0;
 
-        virtual void events(gameEngine::engine* game) = 0;
-        virtual void update(gameEngine::engine* game) = 0;
-        virtual void draw(gameEngine::engine* game) = 0;
+        virtual void events(gameEngine::engine *game) = 0;
+        virtual void update(gameEngine::engine *game) = 0;
+        virtual void draw(gameEngine::engine *game) = 0;
 
-        void changeState(gameEngine::engine* game, state* newState)
-        { game->changeState(newState); }
-
+        void changeState(gameEngine::engine *game, state *newState)
+        {
+            game->changeState(newState);
+        }
     };
-    
+
     state::state(/* args */)
     {
     }
-    
+
     state::~state()
     {
     }
-    
+
 } // namespace gameState
