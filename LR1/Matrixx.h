d52328dfe4703eb6ed25ba4ed6ae5d3767917889
Macviej <blaklovelas@gmail.com>
2022-10-20 11:21:41 +0300

    First kab

---
+++ LR1/Matrixx.h
@@ -0,0 +1,85 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+/*! This class is used to operate with matrixies*/
+
+namespace MMM
+{
+    class Matrixx
+    {
+    public:
+        /// Command for entering matrix from the file.
+        /// @param file This represents name of the file with a matrix.
+        /// @returns Nothing important.
+        void enter(string file); 
+
+        /*! Command for printing the matrix on the screen. */
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void print();
+
+        /*! Command for incrementing matrix.*/
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void operator ++();
+
+        /*! Command for post-incrementing matrix. */
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void operator ++(int);
+
+        /*! Command for decrementing matrix.. */
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void operator --();
+
+        /*! Command for post-decrementing matrix. */
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void operator --(int);
+
+        /*! Command for adding to the matrix the <radok> column, if a = 1, or the <radok> row. */
+        /// @param a represents the key for adding a coloumn.
+        /// @param b represents the key for adding a row.
+        /// @param radok represents the added line.
+        /// @returns Nothing important.
+        void add(bool a, bool b, vector<int> radok);
+
+        /*! Command for deleting the last column, if a = 1, or the row from matrix. */
+        /// @param a represents the key for deleting a coloumn.
+        /// @param b represents the key for deleting a row.
+        /// @returns Nothing important.
+        void del(bool a, bool b);
+
+        /*! Command for extracting submatrix from the matrix.
+        * Which consists of original matrix's elements starting with element with coordinates x1 and y1,
+        * ending with x2, y2, and all elements between them.*/
+        /// @param x1 is a row coordinate of the left extracted matrix's corner.
+        /// @param y1 is a column coordinate of the left extracted matrix's corner.
+        /// @param x2 is a row coordinate of the right extracted matrix's corner.
+        /// @param y2 is a column coordinate of the right extracted matrix's corner.
+        /// @returns Nothing important.
+        void extract(int x1, int y1, int x2, int y2);
+
+        /*! Command for defining the type of matrix. */
+        /// @param No parameters.
+        /// @returns string that consists of words defining the matrix's type.
+        string type();
+
+        /*! Command for transponating matrix. */
+        /// @param No parameters.
+        /// @returns Nothing important.
+        void trans();
+
+        vector<vector<int>> vector_return();
+
+        void operator +(Matrixx m2);
+    };
+}
---
+++ LR1/main.cpp
@@ -0,0 +1,165 @@
+﻿#include "pch.h"
+#include "Matrixx.h"
+
+using namespace MMM;
+
+//Matrix1.txt
+
+void Menu(Matrixx m)
+{
+    while (true)
+    {
+        system("cls");
+
+        int k = 0;
+        cout << "Choose function:" << endl << "1. Enter matrix from a file." << endl <<
+            "2. Print matrix on a screen" << endl <<
+            "3. Matrix++." << endl << "4. ++Matrix." << endl <<
+            "5. Matrix--." << endl << "6. --Matrix." << endl <<
+            "7. Add a Column or a String." << endl <<
+            "8. Delete a Column or a String." << endl <<
+            "9. Extract Matrix." << endl << "10. Define type of Matrix." << endl <<
+            "11. Transporate Matrix." << endl <<"12. End program" << endl <<
+            "----------------------" << endl;
+        cin >> k;
+
+        system("cls");
+
+        switch (k)
+        {
+        case 1:
+            {
+                string FILE;
+                cout << "Enter name of a file: "; cin >> FILE;
+
+                m.enter(FILE);
+                break;
+            }
+        case 2:
+            {
+                m.print();
+
+                system("pause");
+                break;
+            }
+        case 3:
+            {
+                m++; break;
+            }
+        case 4:
+            {
+                ++m; break;
+            }
+        case 5:
+            {
+                m--; break;
+            }
+        case 6:
+            {
+                --m; break;
+            }
+        case 7:
+            {
+                cout << "Add:" << endl << " 1) A Column" << endl << " 2) A String" << endl;
+                int k1 = 0, Temp_Int;
+                vector<int> Temp_Vector;
+                string Temp_String;
+                string Temp_Char;
+                cin >> k1;
+
+                switch (k1)
+                {
+                case 1: 
+                    { 
+                        cout << "Enter a column size: "; int size; cin >> size;
+                        cout << "Enter a column:" << endl;
+                        for (int i = 0; i < size; i++)
+                        {
+                            cin >> Temp_Int;
+                            Temp_Vector.push_back(Temp_Int);
+                        }
+                        
+                        m.add(1, 0, Temp_Vector);
+                            
+                        break;
+                    }
+                case 2:
+                    {
+                        cout << "Enter a column size: "; int size; cin >> size;
+                        cout << "Enter a column:" << endl;
+                        for (int i = 0; i < size; i++)
+                        {
+                            cin >> Temp_Int;
+                            Temp_Vector.push_back(Temp_Int);
+                        }
+
+                            m.add(0, 1, Temp_Vector);
+
+                            break;
+                    }
+                }
+                break;
+
+            }
+        case 8:
+            {
+                cout << "Delete:" << endl << " 1) A Column" << endl << " 2) A String" << endl;
+                int k1 = 0;
+                cin >> k1;
+
+                switch (k1)
+                {
+                case 1:
+                    {
+                        m.del(1, 0);
+
+                        break;
+                    }
+                case 2:
+                    {
+                        m.del(0, 1);
+
+                        break;
+                    }
+                }
+                break;
+            }
+        case 9:
+            {
+                int x1, x2, y1, y2;
+                cout << "Enter coordinates of extracted matrix: " << endl;
+                cin >> x1 >> y1 >> x2 >> y2;
+
+                m.extract(x1, y1, x2, y2);
+                break;
+            }
+        case 10:
+            {
+                cout << m.type() << endl;
+
+                system("pause");
+                break;
+            }
+        case 11:
+            {
+                m.trans();
+                break;
+            }
+        case 12:
+            {
+            return;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    Matrixx m1, m2, m3;
+    vector<int> Temp_Vector = { 1, 2, 3, 4 };
+    string A = "afa";
+    vector<vector<int>> VectorA;
+
+    Menu(m1);
+}
