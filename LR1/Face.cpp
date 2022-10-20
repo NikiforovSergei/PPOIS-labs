bad7e8753fabd22d4324c0716e884d1a746a7276
Max <93283693+MaksimPilat@users.noreply.github.com>
2022-10-20 20:27:04 +0300

    Add files via upload

---
+++ LR1/Face.cpp
@@ -0,0 +1,70 @@
+#include "Face.h"
+#include <vector>
+
+Face::Face()
+{
+	index = 0;
+	naighbor = { {"topFace", 0},
+					{"bottomFace", 0},
+					{"leftFace", 0},
+					{"rightFace", 0}
+	};
+
+}
+
+Face::Face(int valueIndex, vector<vector<int>> &valueVertical, vector<vector<int>> &valueHorizontal)
+{
+	index = valueIndex;
+	if (index == 0)
+	{
+		naighbor = { {"topFace", 1},
+					 {"bottomFace", 3},
+					 {"leftFace", 4},
+					 {"rightFace", 5}
+		};
+	}
+	if (index == 1)
+	{
+		naighbor = { {"topFace", 2},
+					 {"bottomFace", 0},
+					 {"leftFace", 4},
+					 {"rightFace", 5}
+		};
+	}
+	if (index == 2)
+	{
+		naighbor = { {"topFace", 3},
+					 {"bottomFace", 1},
+					 {"leftFace", 5},
+					 {"rightFace", 4}
+		};
+	}
+	if (index == 3)
+	{
+		naighbor = { {"topFace", 0},
+					 {"bottomFace", 2},
+					 {"leftFace", 4},
+					 {"rightFace", 5}
+		};
+	}
+	if (index == 4)
+	{
+		naighbor = { {"topFace", 1},
+					 {"bottomFace", 3},
+					 {"leftFace", 2},
+					 {"rightFace", 0}
+		};
+	}
+	if (index == 5)
+	{
+		naighbor = { {"topFace", 1},
+					 {"bottomFace", 3},
+					 {"leftFace", 0},
+					 {"rightFace", 2}
+		};
+	}
+
+	vertical = valueVertical;
+	horizontal = valueHorizontal;
+
+}
