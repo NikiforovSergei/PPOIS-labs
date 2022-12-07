be50dd11deafb401505fb9cacc410c7133c19efa
Vladimir Moshchuk <ardonplay@Vladimirs-MacBook-Air.local>
2022-12-07 22:44:27 +0300

    add working, but without operators lab

---
+++ LR3/graph.h
@@ -0,0 +1,608 @@
+#pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+
+template <typename type, typename A = std::allocator<type>>
+class graph
+{
+private:
+
+	struct tail
+	{
+		type key;
+		tail* next_leader;
+		tail* next_tail;
+	};
+
+	struct leader
+	{
+		type key;
+		leader* next;
+		tail* list;
+	};
+
+	leader* begin;
+	int vertex_counter;
+	int edges_counter;
+	void print_edges(leader* vertex);
+	leader* find_end();
+	void delete_spisok(tail* value);
+	void delete_tail(type value);
+	
+
+public:
+	typedef std::pair<leader*, tail*> pair;
+	class iterator_node;
+	class iterator_edge;
+	graph();
+	graph(const graph& other);
+	~graph();
+
+	graph& operator=(const graph& other);
+	bool operator ==(const graph& other);
+	bool operator !=(const graph& other);
+	bool operator >=(const graph& other);
+	bool operator <=(const graph& other);
+	bool operator >(const graph& other);
+	bool operator <(const graph& other);
+
+	bool add_vertex(type value);
+	bool add_edge(type first, type second);
+	void print();
+	int get_vertex_counter();
+	int get_edges_counter();
+	int get_vertex_degree(type vertex);
+	bool find_edge(type first, type second);
+	bool delete_edge(type first, type second);
+	bool delete_vertex(type first);
+	bool empty();
+	void clear(leader* value);
+	leader* finder(type value);
+	leader& operator [](const int& n);
+
+	leader* get_begin();
+	leader* get_end();	
+
+	iterator_node Begin() { return get_begin(); }
+	iterator_node End() { return get_end(); }
+	
+	iterator_edge eBegin(iterator_node& x)
+	{ 
+		return x.temp->list;
+	}
+	iterator_edge eEnd() 
+	{ 
+		return nullptr;
+	}
+
+	class iterator_node
+	{
+	public:
+		leader* temp;
+		iterator_node(leader* value) :temp(value)
+		{}
+		leader& operator++(int) { return *(temp = temp->next); }
+		leader& operator++()
+		{
+			leader* t = temp;
+			temp = temp->next;
+			return *t;
+		}
+		bool operator !=(const iterator_node& it) { return temp != it.temp; }
+		leader& operator*() { return *temp; }
+	};
+	
+	class iterator_edge
+	{
+		tail* ttemp;
+	public:
+		iterator_edge(tail* value) :ttemp(value)
+		{}
+		tail& operator++(int) { return *(ttemp=ttemp->next_tail); }
+		tail& operator++()
+		{
+			pair t = ttemp;
+			ttemp=ttemp->next_tail;
+			return *t;
+		}
+		bool operator !=(const iterator_edge& it) { return ttemp != it.ttemp; }
+		tail& operator*(){ return *ttemp; }
+	};
+};
+
+template<typename type,typename A>
+inline graph<type,A>::graph()
+{
+	begin = nullptr;
+	vertex_counter = 0;
+	edges_counter = 0;
+}
+
+template<typename type, typename A>
+inline graph<type,A>::graph(const graph<type,A>& other)
+{
+	leader* temp = other.begin;
+	while (temp)
+	{
+		this->add_vertex(temp->key);		
+		temp = temp->next;
+	}
+	
+	temp = other.begin;
+	while (temp)
+	{
+		tail* ttemp = temp->list;
+		while (ttemp)
+		{
+			this->add_edge(temp->key, ttemp->key);
+			ttemp = ttemp->next_tail;
+		}
+		temp = temp->next;
+	}
+}
+
+template<typename type, typename A>
+inline graph<type,A>::~graph()
+{
+	clear(get_begin());
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::add_vertex(type value)
+{
+	if (vertex_counter)
+	{
+		if (!finder(value))
+		{
+			leader* temp = find_end();
+			temp->next = new leader();
+			temp = temp->next;
+			temp->key = value;
+			temp->next = nullptr;
+			temp->list = nullptr;
+			vertex_counter++;
+			return 1;
+		}
+		std::cout << "Vertex has been added before\n";
+		return 0;
+	}
+	else
+	{
+		begin = new leader();
+		begin->next = nullptr;
+		begin->list = nullptr;
+		begin->key = value;
+		vertex_counter++;
+		return 1;
+	}
+	return 0;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::add_edge(type first, type second)
+{
+	if (finder(first) && finder(second))
+	{
+		if (find_edge(first, second)) return 0;
+		if (first == second) return 0;
+
+		leader* temp = finder(first);
+		if (temp->list)
+		{
+			tail* ttemp = temp->list;
+
+			while (ttemp->next_tail != nullptr)
+				ttemp = ttemp->next_tail;
+
+			ttemp->next_tail = new tail();
+			ttemp = ttemp->next_tail;
+			ttemp->next_tail = nullptr;
+			ttemp->next_leader = finder(second)->list;
+			ttemp->key = second;
+		}
+		else
+		{
+			temp->list = new tail();
+			tail* ttemp = temp->list;
+			ttemp->next_tail = nullptr;
+			ttemp->next_leader = (finder(second))->list;
+			ttemp->key = second;
+		}
+		edges_counter++;
+		return 1;
+	}
+	else
+	{
+		std::cout << "there's no vertex such yours\n";
+		return 0;
+	}
+}
+
+template<typename type, typename A>
+inline void graph<type,A>::print_edges(leader *vertex)
+{
+	tail* ttemp = vertex->list;
+	while (ttemp != nullptr)
+	{
+		std::cout << "\t" << ttemp->key;
+		ttemp = ttemp->next_tail;
+	}
+	std::cout << std::endl;
+}
+
+template<typename type, typename A>
+inline void graph<type,A>::print()
+{
+	leader* temp = begin;
+	while (temp != nullptr)
+	{
+		std::cout << temp->key <<"\n";
+		print_edges(temp);
+		temp = temp->next;
+	}
+	
+}
+
+template<typename type, typename A>
+int graph<type,A>::get_vertex_counter()
+{
+	return vertex_counter;
+}
+
+template<typename type, typename A>
+inline int graph<type,A>::get_edges_counter()
+{
+	return edges_counter;
+}
+
+template<typename type, typename A>
+inline int graph<type,A>::get_vertex_degree(type vertex)
+{
+	leader* temp = finder(vertex);
+	if (temp)
+	{
+		tail* ttemp = temp->list;
+		int counter = 0;
+		while (ttemp)
+		{
+			ttemp = ttemp->next_tail;
+			counter++;
+		}
+		return counter;
+	}
+	else
+		std::cout << "There's no vertex";
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::find_edge(type first, type second)
+{
+	if (finder(first) && finder(second))
+	{
+		tail* ttemp  = finder(first)->list;
+		while (ttemp)
+		{
+			if (ttemp->key == second)
+				return 1;
+			ttemp = ttemp->next_tail;
+		}
+	}
+	return 0;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::delete_edge(type first, type second)
+{
+	if (finder(first) && finder(second))
+	{
+		leader* temp = finder(first);
+		tail* ttemp = temp->list;
+		while (ttemp)
+		{
+			if (ttemp->key == second)
+			{
+				if (ttemp == temp->list)
+				{
+					temp->list = temp->list->next_tail;
+					delete ttemp;
+					edges_counter--;
+					return 1;
+				}
+				if (ttemp->next_tail == nullptr)
+				{
+					tail* prev = temp->list;
+					while (prev->next_tail != ttemp)
+						prev = prev->next_tail;
+					prev->next_tail = nullptr;
+					delete ttemp;
+					edges_counter--;
+					return 1;
+				}
+				else
+				{
+					tail* prev = temp->list;
+					while (prev->next_tail->key != second)
+						prev = prev->next_tail;
+					prev->next_tail = prev->next_tail->next_tail;
+					delete ttemp;
+					edges_counter--;
+					return 1;
+				}
+			}
+			ttemp = ttemp->next_tail;
+		}
+	}
+	return 0;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::delete_vertex(type first)
+{
+	leader* temp = finder(first);
+	if (temp)
+	{
+		if (temp == begin)
+		{
+			delete_tail(first);
+			delete_spisok(temp->list);
+			begin = begin->next;
+			delete temp;
+			vertex_counter--;
+			return 1;
+		}
+		if (temp->next == nullptr)
+		{
+			delete_tail(first);
+			delete_spisok(temp->list);
+			leader* prev = begin;
+			while (prev->next != temp)
+				prev = prev->next;
+			prev->next = nullptr;
+			delete temp;
+			vertex_counter--;
+			return 1;
+		}
+		else
+		{
+			delete_tail(first);
+			delete_spisok(temp->list);
+			leader* prev = begin;
+			while (prev->next != temp)
+				prev = prev->next;
+			prev->next = prev->next->next;
+			delete temp;
+			vertex_counter--;
+			return 1;
+		}
+		
+	}
+	return 0;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::empty()
+{
+	if (this->begin)
+	{
+		std::cout << "Not empty\n";
+		return 1;
+	}
+	else
+		std::cout <<"Empty\n";
+	return 0;
+}
+
+template<typename type,  typename A>
+inline void graph<type,A>::clear(leader* value)
+{
+	while (value != nullptr)
+	{
+		clear(value->next);
+		delete_vertex(value->key);
+		return;
+	}
+}
+
+template<typename type, typename A>
+inline typename graph<type, A>::leader& graph<type, A>::operator[](const int& n)
+{
+	if (n <= vertex_counter && n > 0)
+	{
+		leader* temp = begin;
+		for (int i = 0; temp != nullptr; i++, temp = temp->next)
+		{
+			if (i == n) return *temp;
+		}
+	}
+}
+
+template<typename type, typename A>
+inline  typename graph<type,A>::leader* graph<type,A>::get_begin()
+{
+	return begin;
+}
+
+template<typename type, typename A>
+inline void graph<type,A>::delete_spisok(tail* value)
+{
+	while (value != nullptr)
+	{
+		delete_spisok(value->next_tail);
+		edges_counter--;
+		delete value;
+		return;
+	}
+}
+
+template<typename type, typename A>
+inline void graph<type,A>::delete_tail(type value)
+{
+	if (finder(value))
+	{
+		leader* temp = begin;
+		while (temp)
+		{
+			tail* ttemp = temp->list;
+			while (ttemp)
+			{
+				if (ttemp->key == value)
+				{
+					if (ttemp == temp->list)
+					{
+						temp->list = ttemp->next_tail;
+						delete ttemp;
+						break;
+					}
+					if (ttemp->next_tail == nullptr)
+					{
+						tail* prev = temp->list;
+						while (prev->next_tail != ttemp)
+							prev = prev->next_tail;
+						prev->next_tail = nullptr;
+						delete ttemp;
+						break;
+					}
+					else
+					{
+						tail* prev = temp->list;
+						while (prev->next_tail->key != value)
+							prev = prev->next_tail;
+						prev->next_tail = prev->next_tail->next_tail;
+						delete ttemp;
+						break;
+					}
+				}
+				ttemp = ttemp->next_tail;
+			}
+			temp = temp->next;
+		}
+	}
+	return;
+}
+
+template<typename type, typename A>
+inline typename graph<type, A>::leader* graph<type, A>::finder(type value)
+{
+	if (begin)
+	{
+		leader* temp = begin;
+		while (temp->key != value)
+		{
+			temp = temp->next;
+			if (temp == nullptr) return nullptr;
+		}
+		return temp;
+	}
+	return nullptr;
+}
+
+template<typename type, typename A>
+inline typename graph<type,A>::leader* graph<type,A>::find_end()
+{
+	leader* temp = begin;
+	while (temp->next != nullptr) temp = temp->next;
+	return temp;
+}
+
+template<typename type, typename A>
+inline typename graph<type,A>::leader* graph<type,A>::get_end()
+{
+	leader* temp = begin;
+	while (temp) 
+		temp = temp->next;
+	return temp;
+}
+template<typename type, typename A>
+&graph<type, A>::operator=(const graph &other) {
+  return <#initializer#>;
+}
+template<typename type, typename A>
+inline typename graph<type, A>::operator =(const graph<type,A> &other)
+{
+	if (this->begin != nullptr)
+		this->clear(this->begin);
+
+	leader* temp = other.begin;
+
+	while (temp)
+	{
+		this->add_vertex(temp->key);
+		temp = temp->next;
+	}
+
+	temp = other.begin;
+
+	while (temp)
+	{
+		tail* ttemp = temp->list;
+		while (ttemp)
+		{
+			this->add_edge(temp->key, ttemp->key);
+			ttemp = ttemp->next_tail;
+		}
+		temp = temp->next;
+	}
+	return *this;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator ==(const graph<type,A>& other)
+{
+	leader* temp_this = this->begin;
+	leader* temp_other = other.begin;
+	while (temp_this && temp_other)
+	{
+		if (temp_this->key != temp_other->key)
+			return false;
+		tail* ttemp_this = temp_this->list;
+		tail* ttemp_other = temp_other->list;
+		while (ttemp_this && ttemp_other)
+		{
+			if (ttemp_this->key != ttemp_other->key)
+				return false;
+			ttemp_this = ttemp_this->next_tail;
+			ttemp_other = ttemp_other->next_tail;
+		}
+		temp_this = temp_this->next;
+		temp_other = temp_other->next;
+	}
+	return true;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator !=(const graph& other)
+{
+	return !operator==(other);
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator >=(const graph& other)
+{
+	return this->vertex_counter > other.vertex_counter || operator==(other);
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator <=(const graph& other)
+{
+	return this->vertex_counter < other.vertex_counter || operator==(other);
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator >(const graph& other)
+{
+	return this->vertex_counter > other.vertex_counter;
+}
+
+template<typename type, typename A>
+inline bool graph<type,A>::operator <(const graph& other)
+{
+	return this->vertex_counter < other.vertex_counter;
+}
+
+
+#endif 
+
--- LR3/main.cpp
+++ LR3/main.cpp
@@ -1,247 +1,26 @@
-﻿#include <iostream>
-#include <vector>
-#define TRUE 1
-#define FALSE 0
-using namespace std;
- typedef struct L *Lref;//Тип: указатель на заголовочный узел.
-typedef struct T *Tref;//Тип: указатель на дуговой узел.
-//Описание типа заголовочного узла.
-typedef struct L {
- public:
-  std::string Key;    //Имя заголовочного узла.
-  int Count;  //Количество предшественников.
-  Tref Trail; //Указатель на список смежности, содержащий "последователей".
-  Lref Next;  //Указатель на следующий узел в
-  //списке заголовочных узлов.
-}Leader;
-
-//Описание типа дугового узла.
-typedef struct T {
-  Lref Id;
-  Tref Next;
-} Trailer;
-template<class type>
-class Spisok {
- private:
-  Lref Head; //Указатель на список заголовочных узлов.
-  Lref Tail; //Указатель на фиктивный элемент
-  //в конце списка заголовочных узлов.
-  Lref SearchGraph(type);
-  Lref Search(type);
- public:
-  Spisok() {//Инициализация списка заголовочных узлов.
-    Head = Tail = new (Leader);
-  }
-  ~Spisok(); //Деструктор.
-  void addArc(type, type);
-  void deleteArc(type, type);
-  void PrintGraph();
-  void deleteNode(type);
-  void Free1Graph(Lref *, Lref *);
-  void Free2Graph(Tref *);
-  void addNode(type);
-};
-
-int main() {
-  Spisok <std::string> A;
-
-  //Построение и вывод структуры Вирта.
-  A.addArc("Hello", "hello");
-  A.PrintGraph();
-  cout << endl;
-  A.deleteNode("hi");
-  A.PrintGraph();
-  cout << endl;
-
-}
-template<class type>
-Spisok<type>::~Spisok() {
-  //Очистка структуры Вирта.
-  Lref t = Head;
-  while (t!=Tail) {
-    Free2Graph(&(*t).Trail);
-    t = (*t).Next;
-  }
-  Free1Graph(&Head, &Tail);
-  delete Tail;
-}
-template<class type>
-Lref Spisok<type>::SearchGraph(type w)
-//Функция возвращает указатель на заголовочный узел
-//с ключом w. Если заголовочный узел отсутствует, то он
-//добавляется в список. Head - указатель на структуру Вирта.
+#include <iostream>
+#include "wirth_struct.h"
+int main()
 {
-  Lref h;
+  wirth_struct<int> temp;
+  temp.addNode(2);
+  temp.addNode(1);
+  temp.addArc(0,1);
+  temp.addArc(2,1);
+  wirth_struct<int> temp2(temp);
 
-  h = Head;
-  (*Tail).Key = w;
-  while ((*h).Key!=w) h = (*h).Next;
-  if (h==Tail)
-    //В списке заголовочных узлов нет узла с ключом w.
-    //Поместим его в конец списка Head.
+  auto i = wirth_struct<int>::iterator_node(temp.Begin());
+  std::cout << "\n";
+  while (i != temp.End())
   {
-    Tail = new (Leader);
-    (*h).Count = 0;
-    (*h).Trail = nullptr;
-    (*h).Next = Tail;
-  }
-  return h;
-}
-template<class type>
-Lref Spisok<type>::Search(type w)
-//Функция возвращает указатель на заголовочный узел
-//ключом w. Если узел отсутствует, то функция возвращает NULL .
-{
-  Lref h = Head;
-  (*Tail).Key = w; //Поиск "с барьером".
-  while ((*h).Key!=w) h = (*h).Next;
-  if (h==Tail) //В списке заголовочных узлов нет узла с ключом w.
-    h = nullptr;
-  return h;
-}
-
-template<class type>
-void Spisok<type>::addArc(type x, type y)
-//Добавление дуги (x,y) (если ее не было!) к модифицированной структуре
-//Вирта, соответствующей ориентированному графу Head.
-{
-  Lref p, q;    //Рабочие указатели.
-  Tref t, r;    //Рабочие указатели.
-  bool Res; //Флаг наличия в графе данной дуги.
-
-  //Определим, существует ли в графе дуга (x,y)?
-  p = SearchGraph(x);
-  q = SearchGraph(y);
-  r = (*p).Trail;
-  Res = FALSE;
-  while ((r!=nullptr) && (!Res))
-    if ((*r).Id==q) Res = TRUE; else r = (*r).Next;
-  if (!Res) { //Если дуга отсутствует, то поместим её в граф.
-    t = new (Trailer);
-    (*t).Id = q;
-    (*t).Next = (*p).Trail;
-    (*p).Trail = t;
-    (*q).Count++;
-  }
-}
-
-template<class type>
-void Spisok<type>::deleteArc(type x, type y)
-//Функция возвращает указатель Head на структуру
-//Вирта, соответствующую ориентированному графу
-//и полученную удалением дуги (x,y).
-{
-  Lref p, q;    //Рабочие указатели.
-  Tref t, r;    //Рабочие указатели.
-  bool Res; //Флаг наличия в графе данной дуги.
-
-  //Определим, существует ли в графе дуга (x,y)?
-  p = Search(x);
-  q = Search(y);
-  if ((p!=nullptr) && (q!=nullptr)) { //Вершины x и y в графе есть.
-    r = (*p).Trail;
-    Res = FALSE;
-    while ((r!=nullptr) && (!Res))
-      if ((*r).Id==q) Res = TRUE;
-      else r = (*r).Next;
-    if (Res) {//Если дуга существует, то удалим её.
-      if (r==(*p).Trail) {
-        (*p).Trail = (*(*p).Trail).Next;
-        delete r;
-        (*q).Count--;
-      } else {
-        t = (*p).Trail;
-        while ((*t).Next!=r) t = (*t).Next;
-        (*t).Next = (*(*t).Next).Next;
-        delete r;
-        (*q).Count--;
-      }
+    auto j = wirth_struct<int>::iterator_edge(temp.eBegin(i));
+    while (j != temp.eEnd())
+    {
+      std::cout << (*i).Key << "->"<<(*j).Id->Key<<" ";
+      j++;
     }
+    i++;
   }
-}
+  //temp2.PrintGraph();
 
-template<class type>
-void Spisok<type>::PrintGraph()
-//Вывод модифицированной структуры Вирта, заданной указателем
-//Head и соответствующей ориентированному графу.
-{
-  Lref p; //Рабочий указатель.
-  Tref q; //Рабочий указатель.
-
-  p = Head;
-  while (p!=Tail) {
-    cout << (*p).Key << "((";
-    q = (*p).Trail;
-    while (q!=nullptr) {
-      cout << (*(*q).Id).Key << " ";
-      q = (*q).Next;
-    }
-    cout << "))";
-    p = (*p).Next;
-    cout << " ";
-  }
-}
-template<class type>
-void Spisok<type>::deleteNode(type y)
-//Функция возвращает указатель Head на структуру Вирта,
-//соответствующую графу с удаленной вершиной y.
-{
-  Lref p, q; //Рабочие указатели.
-  Tref r, s; //Рабочие указатели.
-  type x;    //Рабочая переменная.
-  //Удаление всех дуг (x,y), оканчивающихся  в вершине y.
-  p = Head;
-  while (p!=Tail) {
-    x = (*p).Key;
-    deleteArc(x, y);
-    p = (*p).Next;
-  }
-  //Удаление списка смежности вершины y.
-  p = SearchGraph(y);
-  r = (*p).Trail;
-  while (r!=nullptr) {
-    s = r;
-    r = (*r).Next;
-    (*(*s).Id).Count++;
-    delete s;
-  }
-  //Удаление узла, содержащего вершину y, из  списка заголовочных узлов.
-  q = Head;
-  if (q==p) {
-    Head = (*Head).Next;
-    delete q;
-  }
-  else {
-    while ((*q).Next!=p) q = (*q).Next;
-    (*q).Next = (*p).Next;
-    delete p;
-  }
-}
-
-template<class type>
-void Spisok<type>::Free1Graph(Lref *Head, Lref *Tail)
-//Очистка динамической памяти, занятой линейным списком,
-// заданным указателем *Head.
-{
-  if (*Head!=*Tail) {
-    Free1Graph(&(**Head).Next, Tail);
-    delete *Head;
-    *Head = nullptr;
-  }
-}
-
-template<class type>
-void Spisok<type>::Free2Graph(Tref *X)
-//Очистка динамической памяти, занятой линейным списком,
-// заданным указателем *X.
-{
-  if (*X!=nullptr) {
-    Free2Graph(&(**X).Next);
-    delete *X;
-    *X = nullptr;
-  }
-}
-template<class type>
-void Spisok<type>::addNode(type T) {
-  SearchGraph(T);
 }
