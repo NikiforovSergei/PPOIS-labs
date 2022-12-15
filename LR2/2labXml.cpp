fa63f085542008900a810eb0bef0461e168803dc
MikitaNahla <92086780+MikitaNahla@users.noreply.github.com>
2022-12-15 10:32:14 +0300

    Add files via upload

---
+++ LR2/2labXml.cpp
@@ -0,0 +1,13 @@
+﻿#include "XML.h"
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	XML test_XML;
+	test_XML = test_XML.readXmlFromFile("input.txt");
+	cout << test_XML;
+}
