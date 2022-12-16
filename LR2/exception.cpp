29e0728e0929fb8a968d9490ee7805d81dfd221a
Mktoto21 <93986409+Mktoto21@users.noreply.github.com>
2022-12-16 17:04:15 +0300

    Add files via upload

---
+++ LR2/exception.cpp
@@ -0,0 +1,28 @@
+#include "exception.h"
+using namespace std;
+
+Exception::syntax_err::syntax_err(string command) : exception(command.c_str())
+{
+	this->command = command;
+}
+
+Exception::name_err::name_err(string name) : exception(name.c_str())
+{
+	this->name = name;
+}
+
+string Exception::name_err::info()
+{
+	string info = "No such name: " + string(name);
+	return info;
+}
+
+Exception::interpret_err::interpret_err(string message) : exception(message.c_str())
+{
+	this->message = message;
+}
+
+string Exception::interpret_err::info()
+{
+	return message;
+}
